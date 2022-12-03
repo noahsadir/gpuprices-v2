8003549a7e9fa281f0c3205b6a4e8bb7ea3d3afa
Noah Sadir <development.noahsadir@gmail.com>
2022-12-03 20:02:13 +0000

    implement price fetching

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -9,6 +9,10 @@ import Image from 'react-bootstrap/Image'
 import ListGroup from 'react-bootstrap/ListGroup';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import ToggleButton from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
+
+import { Moon, MoonFill } from 'react-bootstrap-icons';
+
 import {
   Chart as ChartJS,
   LinearScale,
@@ -18,10 +22,10 @@ import {
   Legend,
 } from 'chart.js';
 import { Scatter } from 'react-chartjs-2';
-import { Moon, MoonFill } from 'react-bootstrap-icons';
 
 var itemDisplayInfo: DisplayInfo  = require('./itemDisplayInfo.json');
 var data: PriceData = require('./testdata-daily.json');
+var granularData: PriceData = require('./testdata-daily.json');
 
 ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
 
@@ -43,19 +47,43 @@ interface PriceDataItem {
   [key: string]: number; // key = gpu, value = price
 }
 
-
-function fetchPrices(start: number, interval: number) {
-  // fetch prices
-}
-
 function App() {
   var [theme, setTheme]: [string, any] = React.useState("dark");
+  var [didLoad, setDidLoad]: [boolean, any] = React.useState(false);
   console.log(itemDisplayInfo);
+
+  fetchPrices(0, 86400000, (priceData?: PriceData) => {
+    if (priceData != undefined) {
+      data = priceData;
+    }
+    fetchPrices(Date.now() - (86400000 * 30), 900000, (granularPriceData?: PriceData) => {
+      if (granularPriceData != undefined) {
+        granularData = granularPriceData;
+      }
+      setDidLoad(true);
+    });
+  });
 
   return (
     <div className="App" data-theme={theme} style={{display: 'flex', flexFlow: 'column'}}>
       <Toolbar theme={theme} onThemeChange={(newTheme: string) => setTheme(newTheme)}/>
-      <MainContent/>
+      {didLoad ? (
+        <MainContent/>
+      ) : (
+        <div style={{display: 'flex', flexGrow: 1, flexBasis: 0}}>
+          <div style={{display: 'flex', flexGrow: 1}}></div>
+          <div style={{display: 'flex', flexGrow: 0, flexFlow: 'column'}}>
+            <div style={{display: 'flex', flexGrow: 1}}></div>
+            <div style={{display: 'flex', flexGrow: 0}}>
+              <Spinner variant={(theme === "light") ? "primary" : "light"} animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div>
+            <div style={{display: 'flex', flexGrow: 1}}></div>
+          </div>
+          <div style={{display: 'flex', flexGrow: 1}}></div>
+        </div>
+      )}
     </div>
   );
 }
@@ -66,8 +94,8 @@ function Toolbar(props: any) {
       <Container fluid>
         <Navbar.Brand style={{fontWeight: 600}}>GPU Price Tracker</Navbar.Brand>
         <div>
-          <Button variant={(props.theme == "light") ? "outline-primary" : "primary"} style={{maxHeight: 40, maxWidth: 40, minHeight: 40, minWidth: 40, padding: 4, marginRight: 8}} onClick={(e: any) => props.onThemeChange((props.theme == "light") ? "dark" : "light")}>
-            {(props.theme == "light") ? <Moon/> : <MoonFill/>}
+          <Button variant={(props.theme === "light") ? "outline-primary" : "primary"} style={{maxHeight: 40, maxWidth: 40, minHeight: 40, minWidth: 40, padding: 4, marginRight: 8}} onClick={(e: any) => props.onThemeChange((props.theme === "light") ? "dark" : "light")}>
+            {(props.theme === "light") ? <Moon/> : <MoonFill/>}
           </Button>
           <Button style={{maxHeight: 40, maxWidth: 40, minHeight: 40, minWidth: 40, padding: 4}} href="https://www.github.com/noahsadir/gpuprices-v2">
             <Image fluid src="github.png"/>
@@ -82,6 +110,12 @@ function MainContent(props: any) {
   var listItems: any[] = [];
   var [selectedListItem, setSelectedListItem]: [string, any] = React.useState("gtx 1050");
   var [dayCount, setDayCount] = React.useState(365);
+
+  var latestPrice: number | undefined = undefined;
+  var earliestPrice: number | undefined = undefined;
+  var change: number = 1;
+  var changeString: string = "+$0.00 (0.00%)";
+  var changeColor: string = "#ff0000";
 
   const handleListClick = (event: any) => {
     setSelectedListItem(event.currentTarget.value);
@@ -123,12 +157,31 @@ function MainContent(props: any) {
   }
 
   var prices: any[] = [];
-  for (var dateMillis in data) {
+  var priceDataset: PriceData = data;
+  if (dayCount <= 30) {
+    priceDataset = granularData;
+  }
+  for (var dateMillis in priceDataset) {
     if (Number(dateMillis) >= (Date.now() - (86400000 * dayCount))) {
+      if (earliestPrice == undefined) {
+        earliestPrice = priceDataset[dateMillis][selectedListItem];
+      }
       prices.push({
         x: Number(dateMillis),
-        y: data[dateMillis][selectedListItem]
+        y: priceDataset[dateMillis][selectedListItem]
       });
+      latestPrice = priceDataset[dateMillis][selectedListItem];
+    }
+  }
+
+  if (earliestPrice != undefined && latestPrice != undefined) {
+    change = latestPrice - earliestPrice;
+    if (change >= 0) {
+      changeString = "+$" + change.toFixed(2) + " (+" + (((latestPrice / earliestPrice) - 1) * 100).toFixed(2) + "%)";
+      changeColor = "#28a745";
+    } else {
+      changeString = "-$" + Math.abs(change).toFixed(2) + " (-" + ((1 - (latestPrice / earliestPrice)) * 100).toFixed(2) + "%)";
+      changeColor = "#dc3545";
     }
   }
 
@@ -163,6 +216,11 @@ function MainContent(props: any) {
         <div style={{flexGrow: 0, flexBasis: 0}}/>
       </div>
       <div style={{flexGrow: 3, flexBasis: 3, display: 'flex', flexFlow: "column"}}>
+        <div style={{display: 'flex', flexFlow: "column", margin: 0, padding: 8}}>
+          <p style={{fontSize: 28, fontWeight: 800, margin: 0, padding: 0}}>{itemDisplayInfo[selectedListItem].name}</p>
+          <p style={{fontSize: 32, fontWeight: 800, margin: 0, padding: 0}}>{"$" + (latestPrice == undefined ? 0 : latestPrice).toFixed(2)}</p>
+          <p style={{fontSize: 16, fontWeight: 800, margin: 0, padding: 0, color: changeColor}}>{changeString}</p>
+        </div>
         <div style={{flexGrow: 1, flexBasis: 1, display: 'flex'}}>
           <div style={{flex: "1 0 0", display: "flex", flexFlow: "column"}}>
             <div style={{flex: "1 0 0"}}/>
@@ -210,12 +268,42 @@ function SelectButton(props: any) {
   return (
     <Button
       style={props.style}
-      variant={(props.selectedVal == props.value ? "primary" : "secondary")}
+      variant={(props.selectedVal == props.value) ? "primary" : "secondary"}
       value={props.value}
       onClick={(event: any) => props.onClick(event.target.value)}>
         {props.text}
       </Button>
   );
+}
+
+function fetchPrices(start: number, interval: number, callback: (priceData?: PriceData) => void) {
+  const url: string = "/public/fetch_prices.php?from=" + start + "&interval=" + interval;
+
+  var config: any = {
+    method: 'get'
+  };
+
+  var status: number = 400;
+
+  fetch(url, config)
+  .then((response: any) => {
+    status = response.status;
+    return response.json();
+  })
+  .then((data: any) => {
+    if (status === 200) {
+      if (data != null) {
+        callback(data);
+      } else {
+        callback(undefined);
+      }
+    } else {
+      callback(undefined);
+    }
+  })
+  .catch((error: any) => {
+    callback(undefined);
+  });
 }
 
 export default App;
